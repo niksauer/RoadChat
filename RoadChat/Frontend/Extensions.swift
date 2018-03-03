6def8e8067036470097425a3f9d5ee5750e3b92b
Nik Sauer <nik.sauer@me.com>
2018-03-03 15:51:43 +0100

    feature/JSendAPIConnector (#45)
    * add additional assets, add fourth tab for conversations, name cell identifiers lowercase
    
    * change codegen model to category/extension, create classes for each model
    
    * add generic JSendAPIConnector to handle GET/POST/PUT/DELETE requests and transform result to jsend specification, make core data User decodable
    
    * remove gitmodules, add MIT license

---
+++ RoadChat/Frontend/Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Extensions.swift
+//  RoadChat
+//
+//  Created by Niklas Sauer on 02.03.18.
+//  Copyright © 2018 Niklas Sauer. All rights reserved.
+//
+
+import Foundation
+
+extension CodingUserInfoKey {
+    static let context = CodingUserInfoKey(rawValue: "context")
+}
---
+++ RoadChat/Frontend/Model/Community/CommunityMessage.swift
@@ -0,0 +1,14 @@
+//
+//  CommunityMessage.swift
+//  RoadChat
+//
+//  Created by Niklas Sauer on 02.03.18.
+//  Copyright © 2018 Niklas Sauer. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CommunityMessage: NSManagedObject {
+    
+}
