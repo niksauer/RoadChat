9ba563c97359884b76d272cd1eb13f190b93b068
Niklas Sauer <nik.sauer@me.com>
2018-03-08 23:41:52 +0100

    add CoreData saving when app terminates, create core data stack, add ability to create MOUser from RoadChatKit.PublicUser, change date type to non scalar in core data

--- RoadChat/Frontend/Model/User/User.swift
+++ RoadChat/Frontend/Model/User/User.swift
@@ -10,7 +10,34 @@ import Foundation
 import CoreData
 import RoadChatKit
 
+enum UserError: Error {
+    case duplicate
+}
+
 class User: NSManagedObject {
+    
+    class func create(from prototype: RoadChatKit.User.PublicUser, in context: NSManagedObjectContext) throws -> User {
+        let request: NSFetchRequest<User> = User.fetchRequest()
+        request.predicate = NSPredicate(format: "id = %@ AND username = %@", prototype.id, prototype.username)
+        
+        do {
+            let matches = try context.fetch(request)
+            if matches.count > 0 {
+                assert(matches.count >= 1, "Transaction.createTransaction -- Database Inconsistency")
+                throw UserError.duplicate
+            }
+        } catch {
+            throw error
+        }
+        
+        let user = User(context: context)
+        user.id = Int32(prototype.id)
+        user.email = prototype.email
+        user.username = prototype.username
+        user.registry = prototype.registry
+        
+        return user
+    }
     
     static func create(_ user: RegisterRequest, completion: @escaping (Error?) -> Void) {
         let userService = UserService()
