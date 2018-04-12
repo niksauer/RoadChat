f223a2522d493c3110fa9b5b712ae149f105588c
Malcolm Malam <malcolm.tatatalo@gmail.com>
2018-04-12 21:45:33 +0200

    created xib for mapviewcontroller

--- RoadChat/Frontend/ViewController/Shared/LocationViewController.swift
+++ RoadChat/Frontend/ViewController/Shared/LocationViewController.swift
@@ -11,11 +11,14 @@ import MapKit
 import CoreLocation
 
 class LocationViewController: UIViewController, MKMapViewDelegate {
-
+    
+    //MARK: - Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
     // MARK: - Private Properties
     private let location: CLLocation
     private let viewFactory: ViewControllerFactory
-    private var mapView: MKMapView!
+    //private var mapView: MKMapView!
     private var tapped: Bool = false
     
      private let userLocationButton: UIButton!
@@ -34,29 +37,6 @@ class LocationViewController: UIViewController, MKMapViewDelegate {
     
     // MARK: - Customization
     override func viewDidLoad() {
-        userLocationButton = UIButton(type: UIButtonType.custom)
-        userLocationButton.addTarget(self, action: #selector(self.zoomToUserLocation (_:)), for: UIControlEvents.touchUpOutside)
-        userLocationButton.image(for: UIControlState.normal) = #imageLiteral(resourceName: "locate")
-        var buttonFrame = button.frame;
-        buttonFrame.size = CGSizeMake(20, 20);
-        userLocationButton.frame = buttonFrame
-        NSLayoutConstraint.activate([
-            //userLocationButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            userLocationButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 16),
-            userLocationButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 8),
-            //userLocationButton.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            ])
-        
-        self.mapView = MKMapView(frame: view.frame)
-        view.addSubview(mapView)
-        mapView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            mapView.topAnchor.constraint(equalTo: view.topAnchor),
-            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-        ])
         
         let annotation = MKPointAnnotation()
         annotation.coordinate = location.coordinate
@@ -68,13 +48,15 @@ class LocationViewController: UIViewController, MKMapViewDelegate {
         mapView.setRegion(region, animated: true)
     }
     
-    // MARK: - Private Methods
-    @objc func zoomToUserLocation(_ sender: Any) {
+    @IBAction func didPressLocateButton(_ sender: UIButton) {
+        print("zoom zoom")
         mapView.showsUserLocation = true
-        mapView.setCenter(mapView.userLocation.coordinate, animated: true)
         let region = MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03))
         mapView.setRegion(region, animated: true)
+        mapView.setCenter(mapView.userLocation.coordinate, animated: true)
+        
     }
+    
     
 
 }
