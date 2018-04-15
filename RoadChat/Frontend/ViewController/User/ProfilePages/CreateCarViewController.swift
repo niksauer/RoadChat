9052f83dc0bbf1a6150b568798fa4691263a53b0
Malcolm Malam <malcolm.tatatalo@gmail.com>
2018-04-15 23:51:21 +0200

    designed create car page

---
+++ RoadChat/Frontend/ViewController/User/ProfilePages/CreateCarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateCarViewController.swift
+//  RoadChat
+//
+//  Created by Malcolm Malam on 15.04.18.
+//  Copyright © 2018 Niklas Sauer. All rights reserved.
+//
+
+import UIKit
+
+class CreateCarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
