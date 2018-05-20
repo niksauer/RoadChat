22b71dcf3844de358e80a1fcd319432f20c30694
Niklas Sauer <nik.sauer@me.com>
2018-05-20 16:17:21 +0200

    fix #171 by using edge patting via UIEdgeInsets

--- RoadChat/Frontend/ViewController/Shared/GeofenceViewController.swift
+++ RoadChat/Frontend/ViewController/Shared/GeofenceViewController.swift
@@ -108,7 +108,7 @@ class GeofenceViewController: UIViewController, MKMapViewDelegate {
         let newGeofence = MKCircle(center: userLocation, radius: radius)
         let geofenceContainer = MKMapSize(width: newGeofence.boundingMapRect.size.width + 250, height: newGeofence.boundingMapRect.size.height + 250)
         let newVisibleMapRect = MKMapRect(origin: newGeofence.boundingMapRect.origin, size: geofenceContainer)
-        mapView.setVisibleMapRect(newVisibleMapRect, animated: true)
+        mapView.setVisibleMapRect(newVisibleMapRect, edgePadding: UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16), animated: true)
         mapView.add(newGeofence)
         geofence = newGeofence
 
@@ -135,7 +135,7 @@ class GeofenceViewController: UIViewController, MKMapViewDelegate {
         }
 
         let newGeofence = MKCircle(center: userLocation.coordinate, radius: 10000)
-        mapView.setVisibleMapRect(newGeofence.boundingMapRect, animated: true)
+        mapView.setVisibleMapRect(newGeofence.boundingMapRect, edgePadding: UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16), animated: true)
         mapView.add(newGeofence)
         geofence = newGeofence
         setInitialGeofence = true
